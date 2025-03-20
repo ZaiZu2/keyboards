d7bd588c1602943d96409816628c8f605a1a5456
Jakub Kawecki <jakkawecki@gmail.com>
2025-03-20 23:38:56 +0100

    feat: setup all keyboard configs in one place

---
+++ qmk-config/zaizu2/kimiko/rules.mk
@@ -0,0 +1,9 @@
+DYNAMIC_TAPPING_TERM_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+TRI_LAYER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+RGBLIGHT_ENABLE    = no    # Enable keyboard 'old' RGB lightning
+RGB_MATRIX_ENABLE  = yes   # Enable keyboard RGB Matrix lightning
