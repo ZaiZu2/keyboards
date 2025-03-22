95221a437ef616bf4a55eb7bce3b9608fc710446
Jakub Kawecki <jakkawecki@gmail.com>
2025-03-22 19:57:59 +0100

    feat:

--- qmk-config/zaizu2/keymap.c
+++ qmk-config/zaizu2/keymap.c
@@ -41,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * | trans |   !   |   @   |   #   |   $   |   %   |-----------------------------------|   ^   |   &   |   *   |   (   |   )   |CAPWORD|
      * |   `   |   1   |   2   |   3   |   4   |   5   |-----------------------------------|   6   |   7   |   8   |   9   |   0   |   ~   |
      * |  F12  |  F1   |  F2   |  F3   |  F4   |  F5   | trans | trans |---| trans | trans |  F6   |  F7   |  F8   |  F9   |  F10  |  F11  |
-     * ------------------------| trans | trans | trans |_ADJUST| trans |---| trans | trans | trans | trans | trans |------------------------
+     * ------------------------| trans | trans | trans |_ADJUST| trans |---| DELETE| trans | trans | trans | trans |------------------------
      */
     [_LOWER] = LAYOUT(
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                                 KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                               KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, CW_TOGG,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TILD,
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,       _______, _______,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-                                   _______, _______, _______, _______, _______,       _______, MO(_ADJUST), _______, _______, _______
+                                   _______, _______, _______, _______, _______,       KC_DEL,  MO(_ADJUST), _______, _______, _______
     ),
     /* LOWER
      * -------------------------------------------------------------------------------------------------------------------------------------
